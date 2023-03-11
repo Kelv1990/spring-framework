fe56846e0aa24bc8f254ddae268b2916c6fbde4a
“Kelv1990” <674222654@qq.com>
2023-03-11 18:40:09 +0800

    5.1 FactoryBean的使用

--- spring-demo/src/test/java/bean/BeanFactoryTest.java
+++ spring-demo/src/test/java/bean/BeanFactoryTest.java
@@ -1,5 +1,7 @@
 package bean;
 
+import bean.factorybean.Car;
+import bean.factorybean.CarFactoryBean;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
@@ -11,5 +13,11 @@ public class BeanFactoryTest {
 		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
 		MyTestBean bean = (MyTestBean) bf.getBean("myTestBean");
 		System.out.println(bean.getTestStr());
+
+		Car car = (Car) bf.getBean("car");
+		System.out.println(car);
+
+		CarFactoryBean carFactoryBean = (CarFactoryBean) bf.getBean("&car");
+		System.out.println(carFactoryBean);
 	}
 }
